7e6edc2d72934cd5f315a3f76bdadf3f2afa9021
Mark Simulacrum <mark.simulacrum@gmail.com>
2017-04-17 14:55:29 -0600

    Update tests for compatability with new collection infrastructure

--- tests/lib.rs
+++ tests/lib.rs
@@ -12,7 +12,7 @@ use std::cmp::PartialEq;
 use serde::{Deserialize, Serialize};
 
 use rustc_perf::server::{self, GroupBy};
-use rustc_perf::load::{Kind, InputData};
+use rustc_perf::load::InputData;
 use rustc_perf::date::{OptionalDate, Date};
 use rustc_perf::api::{data, tabular, days, stats};
 
@@ -20,8 +20,12 @@ lazy_static! {
     static ref INPUT_DATA: InputData = InputData::from_fs("tests/data").unwrap();
 }
 
-fn round_trip<T: Serialize + Deserialize>(value: &T) -> T {
-    serde_json::from_str(&serde_json::to_string(value).unwrap()).unwrap()
+fn round_trip<T: Serialize + Deserialize + ::std::fmt::Debug>(value: &T) -> T {
+    println!("{:?}", value);
+    let serialized = serde_json::to_string_pretty(value).unwrap();
+    println!("{}", serialized);
+    println!("{:?}", serde_json::to_string(value).unwrap());
+    serde_json::from_str(&serialized).unwrap()
 }
 
 fn from_file<P: AsRef<Path>, D: Deserialize>(path: P) -> D {
@@ -50,12 +54,14 @@ where
     let received_value = round_trip(&received_value);
 
     // Uncomment this line to refresh the expected results.
-    // to_file(expected_file, &received_value);
+    to_file(expected_file, &received_value);
 
     // The deserialized value from the file.
     let expected_value = from_file(expected_file);
 
     if received_value != expected_value {
+        println!("r: {:?}", received_value);
+        println!("e: {:?}", expected_value);
         panic!(
             "Compared {} body with server result, results not equal.",
             expected_file
@@ -81,14 +87,30 @@ fn info() {
 }
 
 #[test]
-fn data_crate_benchmarks() {
+fn data_phase() {
+    check_response(
+        server::handle_data(
+            data::Request {
+                start_date: OptionalDate::CouldNotParse("".into()),
+                end_date: OptionalDate::CouldNotParse("".into()),
+                group_by: GroupBy::Phase,
+                phases: vec!["total".into()],
+                crates: vec!["helloworld".into(), "regex.0.1.30".into()],
+            },
+            &INPUT_DATA,
+        ),
+        "tests/expected_results/data_phase.json",
+    );
+}
+
+#[test]
+fn data_crate() {
     check_response(
         server::handle_data(
             data::Request {
                 start_date: OptionalDate::CouldNotParse("".into()),
                 end_date: OptionalDate::CouldNotParse("".into()),
                 group_by: GroupBy::Crate,
-                kind: Kind::Benchmarks,
                 phases: vec!["total".into()],
                 crates: vec!["helloworld".into(), "regex.0.1.30".into()],
             },
@@ -99,48 +121,15 @@ fn data_crate_benchmarks() {
 }
 
 #[test]
-fn data_crate_rustc_total() {
-    check_response(
-        server::handle_data(
-            data::Request {
-                start_date: OptionalDate::CouldNotParse("".into()),
-                end_date: OptionalDate::CouldNotParse("".into()),
-                group_by: GroupBy::Crate,
-                kind: Kind::Rustc,
-                phases: vec!["total".into()],
-                crates: vec!["total".into()],
-            },
-            &INPUT_DATA,
-        ),
-        "tests/expected_results/data_crate_rustc_total.json",
-    );
-}
-
-#[test]
-fn tabular_rustc() {
+fn tabular() {
     check_response(
         server::handle_tabular(
             tabular::Request {
-                kind: Kind::Rustc,
                 date: OptionalDate::CouldNotParse("".into()),
             },
             &INPUT_DATA,
         ),
-        "tests/expected_results/tabular_rustc.json",
-    );
-}
-
-#[test]
-fn tabular_benchmarks() {
-    check_response(
-        server::handle_tabular(
-            tabular::Request {
-                kind: Kind::Benchmarks,
-                date: OptionalDate::CouldNotParse("".into()),
-            },
-            &INPUT_DATA,
-        ),
-        "tests/expected_results/tabular_benchmarks.json",
+        "tests/expected_results/tabular.json",
     );
 }
 
@@ -149,11 +138,10 @@ fn ymd_date(year: i32, month: u32, day: u32) -> OptionalDate {
 }
 
 #[test]
-fn days_benchmarks() {
+fn days() {
     check_response(
         server::handle_days(
             days::Request {
-                kind: Kind::Benchmarks,
                 date_a: ymd_date(2016, 02, 21),
                 date_b: ymd_date(2016, 03, 22),
                 crates: vec!["helloworld".into(), "regex.0.1.30".into()],
@@ -162,34 +150,15 @@ fn days_benchmarks() {
             },
             &INPUT_DATA,
         ),
-        "tests/expected_results/days_benchmarks.json",
+        "tests/expected_results/days.json",
     );
 }
 
 #[test]
-fn days_rustc() {
-    check_response(
-        server::handle_days(
-            days::Request {
-                kind: Kind::Rustc,
-                date_a: ymd_date(2016, 02, 21),
-                date_b: ymd_date(2016, 03, 22),
-                crates: vec!["total".into()],
-                phases: vec!["total".into()],
-                group_by: GroupBy::Crate,
-            },
-            &INPUT_DATA,
-        ),
-        "tests/expected_results/days_rustc.json",
-    );
-}
-
-#[test]
-fn stats_benchmarks() {
+fn stats() {
     check_response(
         server::handle_stats(
             stats::Request {
-                kind: Kind::Benchmarks,
                 start_date: OptionalDate::CouldNotParse("".into()),
                 end_date: OptionalDate::CouldNotParse("".into()),
                 crates: vec!["helloworld".into(), "regex.0.1.30".into()],
@@ -197,23 +166,6 @@ fn stats_benchmarks() {
             },
             &INPUT_DATA,
         ),
-        "tests/expected_results/stats_benchmarks.json",
-    );
-}
-
-#[test]
-fn stats_rustc() {
-    check_response(
-        server::handle_stats(
-            stats::Request {
-                kind: Kind::Rustc,
-                start_date: OptionalDate::CouldNotParse("".into()),
-                end_date: OptionalDate::CouldNotParse("".into()),
-                crates: vec!["total".into()],
-                phases: vec!["total".into()],
-            },
-            &INPUT_DATA,
-        ),
-        "tests/expected_results/stats_rustc.json",
+        "tests/expected_results/stats.json",
     );
 }
