c0eba6ecece6cac677c41707a7e68e09da5ea97d
Mark Simulacrum <mark.simulacrum@gmail.com>
2017-07-31 18:42:39 -0600

    Remove benchmarks/scripts

--- benchmarks/scripts/dirs.sh
+++
@@ -1,5 +0,0 @@
-export TIMES_DIR=$HOME/times
-export RUST_DIR=$HOME/rust
-export BENCH_DIR=$HOME/benchmarks
-export SCRIPTS_DIR=$HOME/benchmarks/scripts
-
--- benchmarks/scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-# We need openssl on Ubuntu (libssl-dev)
-
-# Setup GitHub user
-git config --global user.name "$USERNAME"
-git config --global user.email "$EMAIL"
-git config --global credential.helper store
