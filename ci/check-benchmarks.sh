1183fa6501d7051902dc44c08dfec43709e36654
Mark Rousskov <mark.simulacrum@gmail.com>
2019-10-02 13:56:30 -0400

    Skip self profile collecting on CI

--- ci/check-benchmarks.sh
+++ ci/check-benchmarks.sh
@@ -9,6 +9,7 @@ git clone https://github.com/rust-lang-nursery/rustc-timing.git;
 cargo build -p collector;
 RUST_BACKTRACE=1 RUST_LOG=collector=trace,rust_sysroot=debug \
     cargo run -p collector --bin collector -- \
+    --skip-self-profile \
     --output-repo rustc-timing \
     --exclude servo,cargo,crates.io,packed-simd,sentry-cli,tuple-stress test_benchmarks;
 code=$?
