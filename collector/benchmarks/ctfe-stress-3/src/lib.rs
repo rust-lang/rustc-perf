29964580c665c7d3ca412cf338998595dc250618
Andreas Molzer <andreas.molzer@gmx.de>
2019-10-16 18:49:49 +0200

    Merge ctfe tests into renamed ctfe-stress-3

--- collector/benchmarks/ctfe-stress-3/src/lib.rs
+++ collector/benchmarks/ctfe-stress-3/src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(const_fn, const_let)]
+use std::mem::MaybeUninit;
 
 // Try to make CTFE actually do a lot of computation, without producing a big result.
 // And without support for loops.
@@ -65,3 +66,32 @@ expensive_static!(UNSIZE_TRAIT: &'static Trait = &42u32; [4 16 16 16 16 16]);
 // prone to regressions.
 // 24 is an exponent that makes the repeat expression take less than two seconds to compute
 const FOO: [i32; 1 << 24] = [0; 1 << 24];
+
+// Try CTFE that operate on values that contain largely uninitialized memory, not requiring any
+// particular representation in MIR.
+type LargeUninit = MaybeUninit<[u8; 1 << 23]>;
+
+// copying uninitialized bytes could also be expensive and could be optimized independently, so
+// track regressions here separately. It should also be less costly to compose new values
+// containing largly undef bytes.
+const BAR: LargeUninit = MaybeUninit::uninit();
+
+// Check the evaluation time of passing through a function.
+const fn id<T>(val: T) -> T { val }
+const ID: LargeUninit = id(MaybeUninit::uninit());
+
+const fn build() -> LargeUninit { MaybeUninit::uninit() }
+const BUILD: LargeUninit = build();
+
+// Largely uninitialized memory but initialized with tag at the start, in both cases.
+const NONE: Option<LargeUninit> = None;
+const SOME: Option<LargeUninit> = Some(MaybeUninit::uninit());
+
+// A large uninit surrounded by initialized bytes whose representation is surely computed.
+const SURROUND: (u8, LargeUninit, u8) = (0, MaybeUninit::uninit(), 0);
+const SURROUND_ID: (u8, LargeUninit, u8) = id((0, MaybeUninit::uninit(), 0));
+
+// Check actual codegen for these values.
+pub static STATIC_BAR: LargeUninit = MaybeUninit::uninit();
+pub static STATIC_NONE: Option<LargeUninit> = None;
+pub static STATIC_SOME: Option<LargeUninit> = Some(MaybeUninit::uninit());
