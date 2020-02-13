07dde1a9cbeba9b2e35e28579f945a8913275bf0
Mark Rousskov <mark.simulacrum@gmail.com>
2020-02-13 18:47:20 -0500

    Support recoding commit data into new formats

--- collector/src/bin/recode.rs
+++ collector/src/bin/recode.rs
@@ -1,4 +1,4 @@
-use collector::{ArtifactData, CommitData};
+use collector::{CommitData};
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,33 +8,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             continue;
         }
 
-        if entry.path().extension().map_or(true, |e| e != "bincode") {
-            continue;
-        }
-
         let filename = entry.file_name();
         let filename = filename.to_str().unwrap();
         let file_contents = fs::read(entry.path())?;
-        if filename.starts_with("artifact-") {
-            let contents: ArtifactData = match bincode::deserialize(&file_contents) {
-                Ok(j) => j,
-                Err(err) => {
-                    panic!("Failed to parse for {:?}: {:?}", filename, err);
-                }
-            };
-            let encoded = serde_json::to_string(&contents)?;
-            fs::remove_file(entry.path())?;
-            fs::write(entry.path().with_extension("json"), &encoded)?;
-        } else {
-            let contents: CommitData = match bincode::deserialize(&file_contents) {
+        if filename.starts_with("commit-") && filename.ends_with(".sz") {
+            eprintln!("{:?}", filename);
+            use std::io::Read;
+            let mut out =
+                String::with_capacity(snap::decompress_len(&file_contents).unwrap_or(0));
+            let mut szip_reader = snap::Reader::new(&file_contents[..]);
+            szip_reader.read_to_string(&mut out).unwrap();
+            let file_contents = out.as_str();
+
+            let contents: CommitData = match serde_json::from_str(&file_contents) {
                 Ok(json) => json,
                 Err(err) => {
-                    panic!("Failed to parse for {:?}: {:?}", filename, err);
+                    log::error!("Failed to parse JSON for {}: {:?}", filename, err);
+                    continue;
                 }
             };
-            let encoded = serde_json::to_string(&contents)?;
-            fs::remove_file(entry.path())?;
-            fs::write(entry.path().with_extension("json"), &encoded)?;
+            let mut v = snap::Writer::new(Vec::new());
+            serde_json::to_writer(&mut v, &contents)?;
+            fs::write(entry.path(), v.into_inner()?)?;
         }
     }
 
