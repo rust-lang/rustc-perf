68e948d96078bf37107d4301067a923fc139227c
Mark Rousskov <mark.simulacrum@gmail.com>
2020-02-21 07:54:24 -0500

    Exit if loading data fails

--- site/src/main.rs
+++ site/src/main.rs
@@ -9,6 +9,7 @@
 
 use env_logger;
 
+use futures::future::FutureExt;
 use parking_lot::RwLock;
 use site::{load, util};
 use std::env;
@@ -26,13 +27,37 @@ async fn main() {
     let data_ = data.clone();
     let fut = tokio::task::spawn(async move {
         *data_.write() = Some(Arc::new(
-            load::InputData::from_fs(&util::get_repo_path().unwrap()).await.unwrap(),
+            load::InputData::from_fs(&util::get_repo_path().unwrap())
+                .await
+                .unwrap(),
         ));
-    });
+    })
+    .fuse();
     let port = env::var("PORT")
         .ok()
         .and_then(|x| x.parse().ok())
         .unwrap_or(2346);
     println!("Starting server with port={:?}", port);
-    futures::join!(fut, site::server::start(data, port)).0.unwrap();
+
+    let server = site::server::start(data, port).fuse();
+    futures::pin_mut!(server);
+    futures::pin_mut!(fut);
+    loop {
+        futures::select! {
+            s = server => {
+                eprintln!("Server completed unexpectedly.");
+                return;
+            }
+            l = fut => {
+                if let Err(e) = l {
+                    eprintln!("Loading failed, exiting.");
+                    if let Ok(panic) = e.try_into_panic() {
+                        std::panic::resume_unwind(panic);
+                    }
+                } else {
+                    eprintln!("Loading complete.");
+                }
+            }
+        }
+    }
 }
