e340b1786c3914aca426dfc3799543e9312fb3e1
Mark Rousskov <mark.simulacrum@gmail.com>
2020-06-28 19:19:20 -0400

    Refactor benchmark checking in CI

--- ci/check-benchmarks.sh
+++ ci/check-benchmarks.sh
@@ -6,10 +6,11 @@ bash -c "while true; do sleep 30; echo \$(date) - running ...; done" &
 PING_LOOP_PID=$!
 trap - ERR
 cargo build -p collector;
-RUST_BACKTRACE=1 RUST_LOG=collector=trace,rust_sysroot=debug \
+RUST_BACKTRACE=1 RUST_LOG=collector=debug,rust_sysroot=debug \
     cargo run -p collector --bin collector -- \
     --db temporary.db \
-    --exclude servo,cargo,crates.io,packed-simd,sentry-cli,tuple-stress test_benchmarks;
+    $COLLECTOR_ARGS \
+    test_benchmarks;
 code=$?
 kill $PING_LOOP_PID
 exit $code
