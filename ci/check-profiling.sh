67994a871b70f8f7b26c9c22a8187f6d2aad1394
Nicholas Nethercote <nnethercote@mozilla.com>
2020-07-24 14:30:45 +1000

    Add testing for `collector llvm-lines`.

--- ci/check-profiling.sh
+++ ci/check-profiling.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# This script is basically just a smoke test. It only tests `eprintln`
-# profiling because setting up the other profilers is something of a hassle.
+# This script only tests some of the profilers at the moment. More coverage
+# would be nice.
 
 set -e -x;
 
@@ -27,6 +27,19 @@ test -f results/eprintln-Test-helloworld-Check-Full
 test -f results/eprintln-Test-helloworld-Debug-Full
 test -f results/eprintln-Test-helloworld-Opt-Full
 test ! -e results/eprintln-Test-helloworld-Doc-Full
+
+# Profile with llvm-lines.
+RUST_BACKTRACE=1 RUST_LOG=collector_raw_cargo=trace,collector=debug,rust_sysroot=debug \
+    cargo run -p collector --bin collector -- \
+    profile_local llvm-lines $bindir/rustc Test \
+        --builds Debug \
+        --cargo $bindir/cargo \
+        --include futures \
+        --runs Full
+
+# Check the output is present and looks something like it should.
+test -f results/ll-Test-futures-Debug-Full
+grep -q "Lines.*Copies.*Function name" results/ll-Test-futures-Debug-Full
 
 kill $PING_LOOP_PID
 exit 0
