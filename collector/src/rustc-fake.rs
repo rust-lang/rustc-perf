a7e8e02e78e5ebee9be0dd9bbf5072f97515b079
Mark Rousskov <mark.simulacrum@gmail.com>
2020-07-29 14:12:56 -0400

    Remove unused variable

--- collector/src/rustc-fake.rs
+++ collector/src/rustc-fake.rs
@@ -229,7 +229,7 @@ fn main() {
                 panic!("unknown wrapper: {}", wrapper);
             }
         }
-    } else if let Some(pos) = args.iter().position(|arg| arg == "--skip-this-rustc") {
+    } else if let Some(_) = args.iter().position(|arg| arg == "--skip-this-rustc") {
         // do nothing
     } else {
         if env::var_os("EXPECT_ONLY_WRAPPED_RUSTC").is_some() {
