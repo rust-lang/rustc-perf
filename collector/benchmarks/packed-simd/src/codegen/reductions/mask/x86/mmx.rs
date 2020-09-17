b27f7ac23c7ee5cc29cbddcb70e1d22937f319dd
Mark Rousskov <mark.simulacrum@gmail.com>
2020-09-17 17:37:33 -0400

    Update packed-simd for rust-lang/stdarch#890

--- collector/benchmarks/packed-simd/src/codegen/reductions/mask/x86/mmx.rs
+++ collector/benchmarks/packed-simd/src/codegen/reductions/mask/x86/mmx.rs
@@ -7,27 +7,14 @@ macro_rules! x86_m8x8_mmx_impl {
             #[inline]
             #[target_feature(enable = "mmx")]
             unsafe fn all(self) -> bool {
-                #[cfg(target_arch = "x86")]
-                use crate::arch::x86::_mm_movemask_pi8;
-                #[cfg(target_arch = "x86_64")]
-                use crate::arch::x86_64::_mm_movemask_pi8;
-                // _mm_movemask_pi8(a) creates an 8bit mask containing the most
-                // significant bit of each byte of `a`. If all bits are set,
-                // then all 8 lanes of the mask are true.
-                _mm_movemask_pi8(crate::mem::transmute(self))
-                    == u8::max_value() as i32
+                unimplemented!()
             }
         }
         impl Any for $id {
             #[inline]
             #[target_feature(enable = "mmx")]
             unsafe fn any(self) -> bool {
-                #[cfg(target_arch = "x86")]
-                use crate::arch::x86::_mm_movemask_pi8;
-                #[cfg(target_arch = "x86_64")]
-                use crate::arch::x86_64::_mm_movemask_pi8;
-
-                _mm_movemask_pi8(crate::mem::transmute(self)) != 0
+                unimplemented!()
             }
         }
     };
