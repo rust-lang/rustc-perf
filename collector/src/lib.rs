b15355a1d198c6224b2a08a68fa9dcf3e9a7d75a
Jakub Beránek <berykubik@gmail.com>
2022-10-02 09:17:51 +0200

    Do not include try commits in artifact range
    Now only master commits will be displayed e.g. on the main page of compare if no bounds are specified.

--- collector/src/lib.rs
+++ collector/src/lib.rs
@@ -37,10 +37,10 @@ impl Bound {
     pub fn left_match(&self, commit: &Commit) -> bool {
         match self {
             Bound::Commit(sha) => commit.sha == **sha,
-            Bound::Date(date) => commit.date.0.naive_utc().date() >= *date,
+            Bound::Date(date) => commit.is_master() && commit.date.0.naive_utc().date() >= *date,
             Bound::None => {
                 let last_month = chrono::Utc::now().date().naive_utc() - chrono::Duration::days(30);
-                last_month <= commit.date.0.naive_utc().date()
+                commit.is_master() && last_month <= commit.date.0.naive_utc().date()
             }
         }
     }
@@ -49,8 +49,8 @@ impl Bound {
     pub fn right_match(&self, commit: &Commit) -> bool {
         match self {
             Bound::Commit(sha) => commit.sha == **sha,
-            Bound::Date(date) => commit.date.0.date().naive_utc() <= *date,
-            Bound::None => true,
+            Bound::Date(date) => commit.is_master() && commit.date.0.date().naive_utc() <= *date,
+            Bound::None => commit.is_master(),
         }
     }
 }
