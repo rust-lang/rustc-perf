e49dcc16e33df7601106092d1127167020805a43
Jakub Beránek <berykubik@gmail.com>
2023-01-30 09:19:45 +0100

    Add comment about unifying types to Profile

--- collector/src/benchmark/profile.rs
+++ collector/src/benchmark/profile.rs
@@ -1,3 +1,10 @@
+/// Type of compilation used for benchmarking/profiling.
+// Note: This type is very similar to the definition of a profile in the database crate.
+// However, these types should not be unified, as they serve slightly different purposes.
+// This type is used for specifying profiles to be benchmarked using the CLI, which is not relevant
+// to the database crate.
+// In general, the database versions of types used in the collector should be considered a DB
+// implementation detail, as they may change when we alter database layout.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, clap::ArgEnum, serde::Deserialize)]
 #[clap(rename_all = "PascalCase")]
 pub enum Profile {
