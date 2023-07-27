495fb6c2e2be5c561d2e09927d113e91f5bad589
Jakub Beránek <berykubik@gmail.com>
2023-07-27 10:03:56 +0200

    Add kind column and change table positioning

--- site/frontend/src/pages/compare/artifact-size/artifact-size-table.vue
+++ site/frontend/src/pages/compare/artifact-size/artifact-size-table.vue
@@ -13,24 +13,25 @@ const props = defineProps<{
   b: ArtifactDescription;
 }>();
 
-// Sort binaries first, libraries later. Then within each category, sort in descending order by size
-// of the original (a) artifact component.
-const components = Object.entries(props.a.component_sizes)
-  .sort((a, b) => {
-    const aLib = a[0].startsWith("lib");
-    const bLib = b[0].startsWith("lib");
-    if (aLib && !bLib) {
-      return 1;
-    } else if (!aLib && bLib) {
-      return -1;
-    } else {
-      return b[1] - a[1];
-    }
-  })
-  .map((item) => item[0]);
+// Sort binaries first, libraries later. Then within each category, sort alphabetically.
+const components = Object.keys(props.a.component_sizes).sort((a, b) => {
+  const aLib = a.startsWith("lib");
+  const bLib = b.startsWith("lib");
+  if (aLib && !bLib) {
+    return 1;
+  } else if (!aLib && bLib) {
+    return -1;
+  } else {
+    return a.localeCompare(b);
+  }
+});
+
+function isLibrary(component: string): boolean {
+  return component.startsWith("lib");
+}
 
 function formatName(component: string): string {
-  if (component.startsWith("lib")) {
+  if (isLibrary(component)) {
     return `${component}.so`;
   }
   return component;
@@ -85,69 +86,84 @@ function generateTitle(component: string): string {
 
 <template>
   <div class="category-title">Artifact component sizes</div>
-  <table>
-    <thead>
-      <tr>
-        <th>Component</th>
-        <th>Before</th>
-        <th>After</th>
-        <th>Change</th>
-        <th>% Change</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="component in components">
-        <td class="component" :title="generateTitle(component)">
-          {{ formatName(component) }}
-          <Tooltip>{{ generateTitle(component) }}</Tooltip>
-        </td>
-        <td>
-          <div class="aligned">
-            {{ formatValue(a.component_sizes[component]) }}
-          </div>
-        </td>
-        <td>
-          <div class="aligned">
-            {{ formatValue(b.component_sizes[component]) }}
-          </div>
-        </td>
-        <td
-          :class="
-            getClass(a.component_sizes[component], b.component_sizes[component])
-          "
-        >
-          <div class="aligned">
-            {{
-              formatChange(
+  <div class="wrapper">
+    <table>
+      <thead>
+        <tr>
+          <th>Component</th>
+          <th>Kind</th>
+          <th class="aligned-header">Before</th>
+          <th class="aligned-header">After</th>
+          <th class="aligned-header">Change</th>
+          <th class="aligned-header">% Change</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="component in components">
+          <td class="component">
+            {{ formatName(component) }}
+            <Tooltip>{{ generateTitle(component) }}</Tooltip>
+          </td>
+          <td>
+            {{ isLibrary(component) ? "Library" : "Binary" }}
+          </td>
+          <td>
+            <div class="aligned">
+              {{ formatValue(a.component_sizes[component]) }}
+            </div>
+          </td>
+          <td>
+            <div class="aligned">
+              {{ formatValue(b.component_sizes[component]) }}
+            </div>
+          </td>
+          <td
+            :class="
+              getClass(
                 a.component_sizes[component],
                 b.component_sizes[component]
               )
-            }}
-          </div>
-        </td>
-        <td
-          :class="
-            getClass(a.component_sizes[component], b.component_sizes[component])
-          "
-        >
-          <div class="aligned">
-            {{
-              formatPercentChange(
+            "
+          >
+            <div class="aligned">
+              {{
+                formatChange(
+                  a.component_sizes[component],
+                  b.component_sizes[component]
+                )
+              }}
+            </div>
+          </td>
+          <td
+            :class="
+              getClass(
                 a.component_sizes[component],
                 b.component_sizes[component]
               )
-            }}
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+            "
+          >
+            <div class="aligned">
+              {{
+                formatPercentChange(
+                  a.component_sizes[component],
+                  b.component_sizes[component]
+                )
+              }}
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.wrapper {
+  display: flex;
+  justify-content: center;
+}
 table {
   table-layout: fixed;
-  width: 100%;
   margin-top: 10px;
 
   td,
@@ -166,6 +182,9 @@ table {
     @media (min-width: 600px) {
       width: 120px;
     }
+  }
+  .aligned-header {
+    text-align: right;
   }
 }
 </style>
