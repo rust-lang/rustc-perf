51b79823914343b168458cb0c482327204d98ed1
Daksh Kaushik <dakshkaushik20040126@gmail.com>
2024-03-26 00:07:26 +0530

    "resolved the comments"

--- collector/runtime-benchmarks/async/src/main.rs
+++ collector/runtime-benchmarks/async/src/main.rs
@@ -1,8 +1,7 @@
 use benchlib::benchmark::run_benchmark_group;
-use std::time::Instant;
-use tokio::runtime::Runtime;
+use tokio::runtime::Builder;
 use std::fs::File;
-use std::io::{self, BufRead, BufReader};
+use std::io::{BufRead, BufReader};
 use flate2::read::GzDecoder;
 
 async fn total_char_count(reader: BufReader<GzDecoder<File>>,x: &mut usize) {
@@ -16,18 +15,11 @@ async fn line_char_count(line: String) -> usize {
     line_count
 }
 
-async fn async_operation() -> (usize, u128) {
-    let start_time = Instant::now();
-
-    let file = File::open("./collector/runtime-benchmarks/data/sherlock.txt.gz").expect("can't read a file");
-    let decoder = GzDecoder::new(file);
-    let reader2 = BufReader::new(decoder);
+async fn async_operation() -> usize {
+    let reader2 = BufReader::new(GzDecoder::new(File::open("./collector/runtime-benchmarks/data/sherlock.txt.gz").expect("can't read a file")));
     let mut total_char = 0;
     total_char_count(reader2, &mut total_char).await;
-
-    let end_time = Instant::now();
-    let duration = end_time - start_time;
-    (total_char,duration.as_millis())
+    total_char
 }
 
 fn main() {
@@ -36,7 +28,11 @@ fn main() {
             // This closure should prepare data that will be needed for the benchmark (if any),
             // and then return a closure that will actually be benchmarked/profiled.
             // Create a Tokio runtime
-            let rt = Runtime::new().unwrap();
+            let rt = Builder::new_multi_thread()
+                .worker_threads(1) 
+                .enable_all()     
+                .build()
+                .unwrap();
             move || {
                 rt.block_on(async_operation());
             }
