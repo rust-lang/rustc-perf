69d94deaced78ec2ec297f3c9ec82035f5ddd7c9
Daksh Kaushik <dakshkaushik20040126@gmail.com>
2024-03-27 18:22:58 +0530

    Added Cargo.lock and improved the code

--- collector/runtime-benchmarks/async/src/main.rs
+++ collector/runtime-benchmarks/async/src/main.rs
@@ -1,40 +1,40 @@
 use benchlib::benchmark::run_benchmark_group;
 use tokio::runtime::Builder;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use flate2::read::GzDecoder;
+use benchlib::decompress_file;
 
-async fn total_char_count(reader: BufReader<GzDecoder<File>>,x: &mut usize) {
+const TEXT_SHERLOCK: &[u8] = include_bytes!("../../data/sherlock.txt.gz");
+
+async fn total_char_count(reader: String) -> usize{
+    let mut total_char = 0;
     for line in reader.lines() {
-        *x += line_char_count(line.expect("invalid character")).await;
+        total_char += line_char_count(line).await;
     }
+    total_char
 }
 
-async fn line_char_count(line: String) -> usize {
-    let line_count = line.chars().count();
-    line_count
+async fn line_char_count(line: &str) -> usize {
+    let char_count = line.chars().count();
+    char_count
 }
 
 async fn async_operation() -> usize {
-    let reader2 = BufReader::new(GzDecoder::new(File::open("./collector/runtime-benchmarks/data/sherlock.txt.gz").expect("can't read a file")));
-    let mut total_char = 0;
-    total_char_count(reader2, &mut total_char).await;
+    let sherlock_text = String::from_utf8(decompress_file(TEXT_SHERLOCK)).expect("Invalid UTF-8");
+    let total_char = total_char_count(sherlock_text).await;
     total_char
 }
 
 fn main() {
     run_benchmark_group(|group| {
-        group.register_benchmark("Async", || {
+        group.register_benchmark("async-word-count", || {
             // This closure should prepare data that will be needed for the benchmark (if any),
             // and then return a closure that will actually be benchmarked/profiled.
             // Create a Tokio runtime
-            let rt = Builder::new_multi_thread()
-                .worker_threads(1) 
+            let rt = Builder::new_current_thread()
                 .enable_all()     
                 .build()
-                .unwrap();
+                .unwrap(); 
             move || {
-                rt.block_on(async_operation());
+                rt.block_on(async_operation())
             }
         });
     });
