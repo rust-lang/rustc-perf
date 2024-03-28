ab3065f5faf54d7cfa74655dcc8ddc516cae5b25
R4 Cheng <karaburi2023@gmail.com>
2024-03-28 15:24:33 +0800

    remove unused css (moved into tab.vue)

--- site/frontend/src/pages/compare/tabs.vue
+++ site/frontend/src/pages/compare/tabs.vue
@@ -179,51 +179,6 @@ const activeTab: Ref<Tab> = ref(props.initialTab);
   }
 }
 
-.tab {
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  min-width: 200px;
-  min-height: 60px;
-  padding: 5px;
-  text-align: center;
-  border: 2px dotted #cccccc;
-  border-radius: 5px;
-  cursor: pointer;
-
-  &:hover {
-    @extend .selected;
-  }
-
-  @media (min-width: 600px) {
-    &:not(:first-child) {
-      margin-left: 30px;
-    }
-
-    &:not(:last-child) {
-      :after {
-        content: "";
-        position: absolute;
-        right: -17px;
-        border-right: 1px solid black;
-        top: 20%;
-        bottom: 20%;
-      }
-    }
-  }
-}
-
-.title {
-  font-weight: bold;
-  font-size: 1.1em;
-  margin-bottom: 5px;
-}
-
-.selected {
-  border-style: solid;
-  border-color: black;
-}
-
 .table-wrapper {
   table {
     width: 100%;
@@ -233,13 +188,5 @@ const activeTab: Ref<Tab> = ref(props.initialTab);
   th {
     font-weight: normal;
   }
-}
-
-.summary {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  font-size: 0.9em;
-  flex-grow: 1;
 }
 </style>
