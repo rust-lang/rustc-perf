8868473717d2304c8cf1b4b42fc23438dd8dbffc
Daksh Kaushik <dakshkaushik20040126@gmail.com>
2024-04-21 17:24:28 +0530

    I/O operations in memory giving chance to execute asynchronously

--- collector/runtime-benchmarks/async/src/main.rs
+++ collector/runtime-benchmarks/async/src/main.rs
@@ -6,19 +6,24 @@ const TEXT_SHERLOCK: &[u8] = include_bytes!("../../data/sherlock.txt.gz");
 
 async fn total_char_count(reader: String) -> usize{
     let mut total_char = 0;
+    
     for line in reader.lines() {
-        total_char += line_char_count(line).await;
+        let mut vec: Vec<&str> = Vec::new();
+        total_char += line.chars().count();
+        let big :String= line.to_string().repeat(100);
+        push_and_pop(&mut vec, &big).await;
     }
     total_char
 }
 
-async fn line_char_count(line: &str) -> usize {
-    let char_count = line.chars().count();
-    char_count
+
+async fn push_and_pop<'a>(vec: &'a mut Vec<&'a str>, line: &'a String){
+    vec.push(line.as_str());
+    vec.pop().unwrap();
 }
 
 async fn async_operation() -> usize {
-    let sherlock_text = (String::from_utf8(decompress_file(TEXT_SHERLOCK)).expect("Invalid UTF-8")).repeat(20);
+    let sherlock_text = String::from_utf8(decompress_file(TEXT_SHERLOCK)).expect("Invalid UTF-8");
     let total_char = total_char_count(sherlock_text).await;
     total_char
 }
