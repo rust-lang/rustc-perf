34deec24397ce9098303784ef146f67fede13735
R4 Cheng <karaburi2023@gmail.com>
2024-04-27 10:53:53 +0800

    Fix typo

--- site/src/server.rs
+++ site/src/server.rs
@@ -624,7 +624,7 @@ async fn handle_fs_path(
     }
 
     async fn resolve_template(path: &str) -> Vec<u8> {
-        TEMPLATES.get_template(&path).await.unwrap()
+        TEMPLATES.get_template(path).await.unwrap()
     }
 
     let relative_path = path.trim_start_matches('/');
