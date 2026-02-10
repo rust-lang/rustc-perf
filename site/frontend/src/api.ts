416eb481fdf8f1a222951e72c10a5a9872309fd8
James Barford-Evans <james.barford-evans@arm.com>
2026-02-10 11:14:28 +0000

    sort gives same output across JavaScript impls

--- site/frontend/src/api.ts
+++ site/frontend/src/api.ts
@@ -12,18 +12,20 @@ export interface BenchmarkInfo {
 
 export const DEFAULT_COMPILE_TARGET_TRIPLE = "x86_64-unknown-linux-gnu";
 
+function compareDefaultCompileTarget(a: string, b: string) {
+  return (
+    a === DEFAULT_COMPILE_TARGET_TRIPLE && b !== DEFAULT_COMPILE_TARGET_TRIPLE
+  );
+}
+
 export async function loadBenchmarkInfo(): Promise<BenchmarkInfo> {
   const benchmarkInfo = await getJson<BenchmarkInfo>(INFO_URL);
   benchmarkInfo.compile_targets.sort((a, b) => {
     // Ensure the default target always appears first, then do an alphabetical
     // sort
-    if (a === DEFAULT_COMPILE_TARGET_TRIPLE) {
-      return 2;
-    } else if (a < b) {
-      return 1;
-    } else if (a > b) {
-      return -1;
-    } else return 0;
+    if (compareDefaultCompileTarget(a, b)) return -1;
+    if (compareDefaultCompileTarget(b, a)) return 1;
+    return a.localeCompare(b);
   });
   return benchmarkInfo;
 }
